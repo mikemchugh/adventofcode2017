82147424ab0b34a2cedc2ea28d83c19c0ecf6b32
Mike McHugh <mike.a.mchugh@gmail.com>
2017-12-03 22:37:53 +0000

    Day 2 b

--- advent/src/main/scala/advent/Day02.scala
+++ advent/src/main/scala/advent/Day02.scala
@@ -13,4 +13,20 @@ object Day02 {
 
     numbers.map(row => row.reduceLeft((x, y) => x max y) - row.reduceLeft((x, y) => x min y)).sum
   }
+
+  def divideRow(row: Array[Int]) : Int = {
+    val division = for {
+      a <- row
+      b <- row
+      if a > b && a % b == 0
+    } yield if (a > b && a % b == 0) a / b else 0
+
+    division(0)
+  }
+
+  def checksumEvenlyDivisable(input: String) : Int = {
+    val numbers = convert(input)
+
+    numbers.map(divideRow(_)).sum
+  }
 }
